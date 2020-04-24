1b71999ac15740cd55e2cea0351d5e63f33ae70b
Taylor Artunian <tartunian@gmail.com>
2020-04-23 19:20:00 -0700

    Added Arduino project.

---
+++ Arduino/libraries/filterSensorData/filterSensorData_data.c
@@ -0,0 +1,20 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * filterSensorData_data.c
+ *
+ * Code generation for function 'filterSensorData_data'
+ *
+ */
+
+/* Include files */
+#include "filterSensorData_data.h"
+#include "filterSensorData.h"
+
+/* Variable Definitions */
+dspcodegen_FIRFilter Hd;
+boolean_T isInitialized_filterSensorData = false;
+
+/* End of code generation (filterSensorData_data.c) */
---
+++ Arduino/libraries/filterSensorData/filterSensorData_initialize.h
@@ -0,0 +1,26 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * filterSensorData_initialize.h
+ *
+ * Code generation for function 'filterSensorData_initialize'
+ *
+ */
+
+#ifndef FILTERSENSORDATA_INITIALIZE_H
+#define FILTERSENSORDATA_INITIALIZE_H
+
+/* Include files */
+#include <stddef.h>
+#include <stdlib.h>
+#include "rtwtypes.h"
+#include "filterSensorData_types.h"
+
+/* Function Declarations */
+extern void filterSensorData_initialize(void);
+
+#endif
+
+/* End of code generation (filterSensorData_initialize.h) */
